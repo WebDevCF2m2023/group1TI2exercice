2f1fbc091e2d7e092658f2cf331b10604af31235
Guillaume <guillaumebaillieux88@gmail.com>
2024-02-14 16:21:47 +0100

    maj1402242

--- Modeles/informationsModel.php
+++ Modeles/informationsModel.php
@@ -1,5 +1,5 @@
 <?php
-// Notre fonction qui charge les informations
+
 function getInformations(PDO $db): array
 {
     $sql = "SELECT * FROM informations";
@@ -10,37 +10,28 @@ function getInformations(PDO $db): array
 
 }
 
-// notre fonction qui insert dans informations
+
 function addInformations(PDO $db, string $theid, string $themail, string $themessage, string $thedate): bool|string
 {
-        /*
-     * On récupère les données en assurant leur sécurité
-     *
-     * On utilise la fonction htmlspecialchars pour convertir les caractères spéciaux en entités HTML
-     * Le paramètre ENT_QUOTES permet de convertir les guillemets doubles et simples
-     * On utilise la fonction strip_tags pour supprimer les balises HTML et PHP
-     * On utilise la fonction trim pour supprimer les espaces en début et fin de chaîne
-     */
-
+    
      $theid = htmlspecialchars(strip_tags(trim($theid)), ENT_QUOTES);
-     // false si le courriel n'est pas valide, sinon on le garde
      $themail = filter_var($themail, FILTER_VALIDATE_EMAIL);
      $themessage = htmlspecialchars(strip_tags($themessage), ENT_QUOTES);
      $thedate = htmlspecialchars(strip_tags(trim($thedate)), ENT_QUOTES);
  
-     // si les données ne sont pas valides, on envoie false
+     
      if (empty($theid) || $themail === false || empty($themessage) || empty($thedate)) {
          return false;
      }
-     // on prépare la requête
+     
      $sql = "INSERT INTO informations (theid, themail, themessage, thedate) VALUES ('$theid', '$themail', '$themessage', '$thedate')";
      try {
-         // on exécute la requête
+         
          $db->exec($sql);
-         // si tout s'est bien passé, on renvoie true
+         
          return true;
      } catch (Exception $e) {
-         // sinon, on renvoie le message d'erreur
+         
          return $e->getMessage();
      }
  
--- public/index.php
+++ public/index.php
@@ -1,53 +1,36 @@
 <?php
 
-// Chargement des dépendances
-
-// Connexion à la base de donnée
-
-// Si le formulaire a été envoyé
-
-    // On insert dans la table `informations` si valide
-
-// on récupère toutes les entrées de la table
-// `informations`
-
-// on charge le template qui affiche la vue
-
-// on ferme la connexion 
-
-
-// chargement de configuration (pas de soucis si déjà appelé dans index.php car require_once)
 require_once "../config.php";
-// chargement du modèle de la table comments
-require_once "../model/informationsModel.php";
 
-// connexion à la DB
+require_once "../Modeles/informationsModel.php";
+
+
 try {
-    // création d'une instance de PDO - Connexion à la base de données
+    
     $db = new PDO(MY_DB_DRIVER . ":host=" . MY_DB_HOST . ";dbname=" . MY_DB_NAME . ";charset=" . MY_DB_CHARSET . ";port=" . MY_DB_PORT, MY_DB_LOGIN, MY_DB_PWD);
 } catch (Exception $e) {
     die($e->getMessage());
 }
 
 
-// si le formulaire a été soumis
+
 if (isset($_POST['theid'], $_POST['themail'], $_POST['themessage'], $_POST['thedate'])) {
 
-    // on appelle la fonction d'insertion dans la DB
+    
     $insert = addComments($db, $_POST['theid'], $_POST['themail'], $_POST['themessage'], $_POST['thedate']);
-    // si l'insertion a réussi
+    
     if ($insert) {
-        // on redirige vers la page actuelle
+        
         header("Location: ./?section=livredor");
         exit();
     } else {
-        // sinon, on affiche un message d'erreur
+        
         $message = "Erreur lors de l'insertion";
     }
 }
 
-// on récupère les commentaires
-$comments = getComments($db);
 
-// fermeture de la connexion
+$comments = getInformations($db);
+
+
 $db = null;
